216f30d1e5861fbb1d34ee9613df61633dd5dd3b
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2026-08-11 07:11:55 -0700

    feat(webapp): restore multiplayer play routes

--- webapp/src/PlayPage/PlayPage.tsx
+++ webapp/src/PlayPage/PlayPage.tsx
@@ -1,8 +1,7 @@
 import clsx from 'clsx'
 import { memo } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import env from '~/env'
 import { Footer } from '~/shared/components/Footer/Footer'
 import { LiveTwitchChannels } from '~/shared/components/LiveTwitchChannels/LiveTwitchChannels'
 import { ROUTES_CONFIG } from '~/shared/constants/routes'
@@ -16,8 +15,6 @@ import { Ranked } from './Ranked/Ranked'
 import { Tutorial } from './Tutorial/Tutorial'
 
 export const PlayPage = memo(() => {
-  const isIdentityMode = env.AUTH_MODE === 'google'
-
   return (
     <div
       className={clsx(
@@ -35,42 +32,22 @@ export const PlayPage = memo(() => {
       )}
     >
       <Routes>
-        {isIdentityMode ? (
-          <>
-            <Route
-              element={<Practice />}
-              path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.path}
-            />
-            <Route
-              path="*"
-              element={
-                <Navigate
-                  to={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.routes.BOT.directPath}
-                  replace
-                />
-              }
-            />
-          </>
-        ) : (
-          <>
-            <Route
-              element={<Tutorial />}
-              path={ROUTES_CONFIG.routes.PLAY.routes.TUTORIAL.path}
-            />
-            <Route
-              element={<Ranked />}
-              path={ROUTES_CONFIG.routes.PLAY.routes.RANKED.path}
-            />
-            <Route
-              element={<Practice />}
-              path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.path}
-            />
-            <Route
-              element={<Conquest />}
-              path={ROUTES_CONFIG.routes.PLAY.routes.CONQUEST.path}
-            />
-          </>
-        )}
+        <Route
+          element={<Tutorial />}
+          path={ROUTES_CONFIG.routes.PLAY.routes.TUTORIAL.path}
+        />
+        <Route
+          element={<Ranked />}
+          path={ROUTES_CONFIG.routes.PLAY.routes.RANKED.path}
+        />
+        <Route
+          element={<Practice />}
+          path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.path}
+        />
+        <Route
+          element={<Conquest />}
+          path={ROUTES_CONFIG.routes.PLAY.routes.CONQUEST.path}
+        />
       </Routes>
       <div className={Sprinkles({ width: 'full', marginTop: 'auto' })}>
         <LiveTwitchChannels />
--- webapp/src/PlayPage/Practice/Practice.tsx
+++ webapp/src/PlayPage/Practice/Practice.tsx
@@ -2,10 +2,9 @@ import { GameMode } from '@opensky/proto'
 import clsx from 'clsx'
 import { memo, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { push } from 'redux-first-history'
 
-import env from '~/env'
 import { ToggleButton } from '~/shared/components/ToggleButton'
 import { ToggleButtonGroup } from '~/shared/components/ToggleButtonGroup'
 import { GameType } from '~/shared/constants/ranks'
@@ -26,7 +25,6 @@ import { PracticeToggleButton } from './Practice.css'
 type ValidPracticeGameModes = GameMode.CHALLENGE_CONSTRUCTED | GameMode.PRACTICE_BOT
 
 export const Practice = memo(() => {
-  const isIdentityMode = env.AUTH_MODE === 'google'
   const [mode, setMode] = useState<ValidPracticeGameModes>(
     window.location.pathname.includes('player')
       ? GameMode.CHALLENGE_CONSTRUCTED
@@ -80,64 +78,46 @@ export const Practice = memo(() => {
         }.webp`}
         isDiscovery={gameType === GameType.DISCOVERY}
       >
-        <GameModeHeader
-          page="PRACTICE"
-          title={t('play.gameModes.header.PRACTICE')}
-          hideButton={isIdentityMode}
-        />
+        <GameModeHeader page="PRACTICE" title={t('play.gameModes.header.PRACTICE')} />
         <GameModeDescription description={t('playPage.PRACTICE.description')} />
-        {!isIdentityMode && (
-          <div
-            className={Sprinkles({
-              width: 'full',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-start',
-              marginTop: '16px'
-            })}
+        <div
+          className={Sprinkles({
+            width: 'full',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            marginTop: '16px'
+          })}
+        >
+          <ToggleButtonGroup
+            height="36px"
+            colorType="default"
+            value={mode}
+            onChange={onTypeToggle}
           >
-            <ToggleButtonGroup
-              height="36px"
-              colorType="default"
-              value={mode}
-              onChange={onTypeToggle}
-            >
-              <ToggleButton
-                className={PracticeToggleButton}
-                value={GameMode.PRACTICE_BOT}
-                text={t('general.BOT')}
-                disabled={isInQueue}
-              />
-              <ToggleButton
-                className={PracticeToggleButton}
-                value={GameMode.CHALLENGE_CONSTRUCTED}
-                text={t('general.PVP')}
-                disabled={isInQueue}
-              />
-            </ToggleButtonGroup>
-          </div>
-        )}
+            <ToggleButton
+              className={PracticeToggleButton}
+              value={GameMode.PRACTICE_BOT}
+              text={t('general.BOT')}
+              disabled={isInQueue}
+            />
+            <ToggleButton
+              className={PracticeToggleButton}
+              value={GameMode.CHALLENGE_CONSTRUCTED}
+              text={t('general.PVP')}
+              disabled={isInQueue}
+            />
+          </ToggleButtonGroup>
+        </div>
         <Routes>
           <Route
             path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.routes.BOT.path}
             element={<PracticeVsBotPage />}
           />
-          {isIdentityMode ? (
-            <Route
-              path="*"
-              element={
-                <Navigate
-                  to={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.routes.BOT.directPath}
-                  replace
-                />
-              }
-            />
-          ) : (
-            <Route
-              path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.routes.PLAYER.path}
-              element={<PracticeVsPlayerPage setParentGameType={setGameType} />}
-            />
-          )}
+          <Route
+            path={ROUTES_CONFIG.routes.PLAY.routes.PRACTICE.routes.PLAYER.path}
+            element={<PracticeVsPlayerPage setParentGameType={setGameType} />}
+          />
         </Routes>
       </PlayPageInner>
     </div>
